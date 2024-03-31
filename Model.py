f7c4a1310790a9acffd08612ca867d701c0ea768
aakashpanch <161831833+aakashpanch@users.noreply.github.com>
2024-03-31 17:20:01 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,13 +1,20 @@
+# Define a metamodel dictionary containing node and edge types
 metamodel_dict = {
+    # Define node types
     "nodes": [
         {
+            # Define a node type: Person
             "type": "Person",
+            # Specify relationships associated with the Person node type
             "edges": ["Friends With", "Parent of", "Child of", "Sibling of", "Colleague of"],
         },
         {
-            "type": "pets",
-            "edges": ["owned by"]
+            # Define a node type: Pet
+            "type": "Pet",
+            # Specify relationship associated with the Pet node type
+            "edges": ["Owned by"]
         },
     ],
+    # Define edge types
     "edges": ["Input for", "Outputs", "Executed by", "Connected to",]
 }
